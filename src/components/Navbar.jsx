10ee31fdcf24e9683f1ff3d9b99db39e6b1fea26
Shamilvm <162104443+shamil-global@users.noreply.github.com>
2025-03-07 22:01:07 +0530

    navbar updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
+import { navLinks } from "../constants";
+
+const NavItems = () => {
+  return (
+    <ul className="flex flex-col items-center gap-4 sm:flex-row md:gap-6 relative z-20">
+      {navLinks.map((item) => (
+        <li
+          key={item.id}
+          className="text-neutral-400 hover:text-white sm:hover:bg-black-500 sm:w-full sm:rounded-md py-2 sm:px-5"
+        >
+          <a
+            href={item.href}
+            onClick={() => {}}
+            className="text-lg md:text-base hover:text-white transition-colors"
+          >
+            {item.name}
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 const Navbar = () => {
-  return <header className="fixed top-0 right-0 left-0 z-50">Navbar</header>;
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen((prev) => !prev);
+  return (
+    <header className="fixed top-0 right-0 left-0 z-50 bg-black/90">
+      <div className="max-w-7xl mx-auto">
+        <div className="flex justify-between items-center py-5 mx-auto c-space">
+          <a
+            href="/"
+            className="text-neutral-400 font-bold hover:text-white text-xl transition-colors"
+          >
+            Shamil Vm
+          </a>
+          <button
+            onClick={toggleMenu}
+            className="text-neutral-400 hover:text-white focus:outline-none sm:hidden flex"
+            aria-label="Toggle Menu"
+          >
+            <img
+              src={isOpen ? "assets/close.svg" : "assets/menu.svg"}
+              alt=""
+              className="w-6 h-6"
+            />
+          </button>
+          <nav className="sm:flex hidden">
+            <NavItems />
+          </nav>
+        </div>
+      </div>
+      <div
+        className={`absolute left-0 right-0 bg-black-200 backdrop-blur-sm transition-all duration-300 ease-in-out overflow-hidden z-20 mx-auto sm:hidden block ${
+          isOpen ? "max-h-screen" : "max-h-0"
+        }`}
+      >
+        <nav className="p-5">
+          <NavItems />
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default Navbar;
