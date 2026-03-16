181c37e920110975759fc434ac416b310871c865
Shamil Vm <98647028+Shamilvm@users.noreply.github.com>
2026-03-16 16:08:18 +0400

    resume download update

--- src/sections/resume/Contact.jsx
+++ src/sections/resume/Contact.jsx
@@ -1,8 +1,9 @@
-import { FileText, Github, Linkedin, Mail } from "lucide-react";
-import React from "react";
-import { Link } from "react-router-dom";
+import { FileText, Mail } from "lucide-react";
 
 export function Contact({ data }) {
+  const handleChange = (url) => {
+    window.open(url, "_blank");
+  };
   return (
     <div className="py-10">
       <div className="space-y-6 text-center">
@@ -23,13 +24,18 @@ export function Contact({ data }) {
             <Mail className="mr-2 w-4 h-4" />
             Get in touch
           </button>
-          <button
-            className="inline-flex justify-center items-center hover:bg-primary/10 disabled:opacity-50 shadow px-4 border border-primary rounded-md focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring h-9 font-medium text-primary text-sm whitespace-nowrap transition-colors cursor-target disabled:pointer-events-none"
-            onClick={() => handleChange(data.RESUME)}
+          <a
+            href="/resume/MUHAMMED SHAMIL V M.pdf"
+            download="MUHAMMED SHAMIL V M.pdf"
           >
-            <FileText className="mr-2 w-4 h-4" />
-            Download Resume
-          </button>
+            <button
+              className="inline-flex justify-center items-center hover:bg-primary/10 disabled:opacity-50 shadow px-4 border border-primary rounded-md focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring h-9 font-medium text-primary text-sm whitespace-nowrap transition-colors cursor-target disabled:pointer-events-none"
+              // onClick={() => handleChange(data.RESUME)}
+            >
+              <FileText className="mr-2 w-4 h-4" />
+              Download Resume
+            </button>
+          </a>
         </div>
       </div>
     </div>
--- src/sections/resume/Intro.jsx
+++ src/sections/resume/Intro.jsx
@@ -1,10 +1,11 @@
-import { Github, Linkedin, Mail } from "lucide-react";
+import { Download, Github, Linkedin, Mail } from "lucide-react";
 import { MovingElement } from "../../components/ui/moving-element";
 
 export function Intro({ data }) {
   const handleChange = (url) => {
     window.open(url, "_blank");
   };
+
   return (
     <div id="intro" className="pt-32 space-y-2">
       <p className="font-normal text-base">hi there👋, I&apos;m</p>
@@ -20,14 +21,21 @@ export function Intro({ data }) {
       </div>
 
       <div className="flex items-center gap-4 text-sm">
-        <MovingElement
-          className="flex w-full justify-center items-center bg-primary betterhover:hover:bg-primary/90 disabled:opacity-50 shadow px-4 py-2 rounded-md focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring h-9 font-medium text-black text-sm transition-colors disabled:pointer-events-none"
-          change={() => handleChange(data.RESUME)}
-          toChange={false}
-          ariaLabel="Resume"
+        <a
+          href="/resume/MUHAMMED SHAMIL V M.pdf"
+          download="MUHAMMED SHAMIL V M.pdf"
         >
-          Resume
-        </MovingElement>
+          <MovingElement
+            className="flex w-full justify-center items-center bg-primary betterhover:hover:bg-primary/90 disabled:opacity-50 shadow px-4 py-2 rounded-md focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring h-9 font-medium text-black text-sm transition-colors disabled:pointer-events-none"
+            // change={() => handleChange(data.RESUME)}
+            toChange={false}
+            ariaLabel="Resume"
+          >
+            <span className="flex gap-2 items-center">
+              Resume <Download size={16} />
+            </span>
+          </MovingElement>
+        </a>
         <div className="flex gap-4">
           <MovingElement
             change={() => handleChange(data.EMAIL)}
